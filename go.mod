8d0296e2bdfd14b32af60a52fb73ce0850adb6a0
ysur67 <rus.muratow@gmail.com>
2021-10-27 10:39:27 +0300

    add bin dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module main.go
 
 go 1.17
+
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/uptrace/bun/driver/pgdriver v1.0.14
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/uptrace/bun v1.0.14 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
+	mellium.im/sasl v0.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"crypto/tls"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/uptrace/bun/driver/pgdriver"
+)
 
 func main() {
-	fmt.Print("asd")
+	godotenv.Load()
+	fgconn := pgdriver.NewConnector(
+		pgdriver.WithNetwork("tcp"),
+		pgdriver.WithAddr(os.Getenv("db-address")),
+		pgdriver.WithTLSConfig(&tls.Config{InsecureSkipVerify: true}),
+		pgdriver.WithUser(os.Getenv("db-username")),
+		pgdriver.WithPassword(os.Getenv("db-password")),
+		pgdriver.WithDatabase(os.Getenv("db-name")),
+		pgdriver.WithTimeout(5*time.Second),
+		pgdriver.WithDialTimeout(5*time.Second),
+		pgdriver.WithReadTimeout(5*time.Second),
+		pgdriver.WithWriteTimeout(5*time.Second),
+	)
+	ctx := context.Background()
+	db, err := fgconn.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
